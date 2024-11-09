12f9c821361eacb3244a715db52097acb81b2d52
manuelVVrepositorioGIT <104441634+manuelVVrepositorioGIT@users.noreply.github.com>
2024-11-08 21:17:58 -0500

    Add files via upload

---
+++ src/main/java/com/mvv/fatima/jsv/JsvAppApplication.java
@@ -0,0 +1,13 @@
+package com.mvv.fatima.jsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvv/fatima/jsv/JsvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvv.fatima.jsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
