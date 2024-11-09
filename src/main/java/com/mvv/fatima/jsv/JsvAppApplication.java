d0f4ded81800ed9b2553ebcf05c91a99d39822b6
manuelVVrepositorioGIT <104441634+manuelVVrepositorioGIT@users.noreply.github.com>
2024-11-09 02:37:30 +0000

    añadido democontroller

--- src/main/java/com/mvv/fatima/jsv/JsvAppApplication.java
+++ src/main/java/com/mvv/fatima/jsv/JsvAppApplication.java
@@ -2,6 +2,8 @@ package com.mvv.fatima.jsv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JsvAppApplication {
@@ -9,5 +11,13 @@ public class JsvAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JsvAppApplication.class, args);
 	}
+}
 
+@RestController
+class DemoController{
+
+	@GetMapping("/hello")
+	public String hello(String name){
+		return "Hello World, and hello to you to " + name;
+	}
 }
